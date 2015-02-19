9a94fbc55eaebebe478630bf1f5f313756af143c
Dieter Reuter <dieter.reuter@me.com>
2015-02-20 00:26:45 +0100

    remove obsolete scripts

--- createtar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -x
-docker run -v /tmp:/tmp --rm=true -ti hypriot/rpi-docker-builder:1.5.0 /bin/bash -c 'cd /src/docker/bundles/1.5.0/dynbinary && tar cvfz /tmp/docker-1.5.0.tar.gz .'
-cp /tmp/docker-1.5.0.tar.gz .
-sudo rm -f /tmp/docker-1.5.0.tar.gz
--- updatedockerbins.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-tar xvf docker-1.5.0.tar.gz ./docker-1.5.0 ./dockerinit-1.5.0
-sudo service docker stop
-sudo cp ./docker-1.5.0 /usr/bin/docker
-sudo cp ./dockerinit-1.5.0 /usr/lib/docker/dockerinit
-sudo service docker start
