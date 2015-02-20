a3fd77198397abb4924f928e881ad9f9d66bf68e
Dieter Reuter <dieter.reuter@me.com>
2015-02-20 20:58:25 +0100

    bump build package to hypriot-3

--- run-builder.sh
+++ run-builder.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-docker run --rm=true --env-file=.env hypriot/rpi-docker-builder /builder.sh 1.5.0 hypriot-2
+docker run --rm=true --env-file=.env hypriot/rpi-docker-builder /builder.sh 1.5.0 hypriot-3
