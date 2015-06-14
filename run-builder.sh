d5bcfc0b5725c2e46e8f4517e85edff7bdba4854
Dieter Reuter <dieter.reuter@me.com>
2015-06-14 13:18:26 +0000

    bump version to 1.7.0-rc3 -2

--- run-builder.sh
+++ run-builder.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 mkdir -p dist
 touch .env
-docker run --rm=true --env-file=.env -v $(pwd)/builder.sh:/builder.sh -v $(pwd)/pkg-debian:/pkg-debian -v $(pwd)/dist:/dist hypriot/rpi-docker-builder /builder.sh 1.7.0-rc3 -1
+docker run --rm=true --env-file=.env -v $(pwd)/builder.sh:/builder.sh -v $(pwd)/pkg-debian:/pkg-debian -v $(pwd)/dist:/dist hypriot/rpi-docker-builder /builder.sh 1.7.0-rc3 -2
