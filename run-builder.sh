a4ed990a7cc8f63f491a163985073ff2d1675009
Dieter Reuter <dieter.reuter@me.com>
2015-11-14 06:15:19 +0000

    bump version to 1.9.1-rc1

--- run-builder.sh
+++ run-builder.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 mkdir -p dist
 touch .env
-docker run --rm=true --env-file=.env -v $(pwd)/builder.sh:/builder.sh -v $(pwd)/pkg-debian:/pkg-debian -v $(pwd)/dist:/dist hypriot/rpi-docker-builder /builder.sh 1.9.0 -2
+docker run --rm=true --env-file=.env -v $(pwd)/builder.sh:/builder.sh -v $(pwd)/pkg-debian:/pkg-debian -v $(pwd)/dist:/dist hypriot/rpi-docker-builder /builder.sh 1.9.1-rc1 -2
